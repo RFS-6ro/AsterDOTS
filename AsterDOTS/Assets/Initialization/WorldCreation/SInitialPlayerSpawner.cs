5b30f3f786ea8cdd75abc96b444e100f1028f250
dtsyvakin <dtsyvakin@gear-games.com>
2022-11-26 20:04:00 +0300

    A-16 Add: main scene, missing references and initial player spawner

---
+++ AsterDOTS/Assets/Initialization/WorldCreation/SInitialPlayerSpawner.cs
@@ -0,0 +1,42 @@
+﻿// ----------------------------------------------------------------------------
+// The Proprietary or MIT License
+// Copyright (c) 2022-2022 RFS_6ro <rfs6ro@gmail.com>
+// ----------------------------------------------------------------------------
+
+using Core.ECS.OneFrame;
+using Core.Utils;
+using Initialization.Configs;
+using Initialization.Configs.EventSystem;
+using Unity.Entities;
+using Update.Spawners.Unit;
+
+namespace Initialization.WorldCreation
+{
+    [RequireMatchingQueriesForUpdate]
+    public partial class SInitialPlayerSpawner : SystemBase
+    {
+        private bool _isInitialized = false;
+        
+        protected override void OnUpdate()
+        {
+            if (_isInitialized) { return; }
+            
+            EntityCommandBuffer entityCommandBuffer = SystemAPI
+                .GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>()
+                .CreateCommandBuffer(EntityManager.WorldUnmanaged);
+            
+            CUnitSpawnRequest request = 
+                new SpawnRequestBuilder(1)
+                    .AddUnit(EntityType.PlayerShip)
+                    .Build();
+            
+            CEventSystemConfig eventSystemConfig = SystemAPI.GetSingleton<CEventSystemConfig>();
+            
+            Entity entity = entityCommandBuffer.Instantiate(eventSystemConfig.EventPrefab);
+            entityCommandBuffer.SetComponent(entity, new COneFrame { FramesActive = 1 });
+            entityCommandBuffer.AddComponent(entity, request);
+
+            _isInitialized = true;
+        }
+    }
+}
