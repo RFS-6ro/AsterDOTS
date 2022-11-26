5c338bd18ee7781103a611fb8a06665f90447ef0
dtsyvakin <dtsyvakin@gear-games.com>
2022-11-26 19:54:39 +0300

    A-11 Add: input listening

---
+++ AsterDOTS/Assets/Update/Input/SReadKeyboardInput.cs
@@ -0,0 +1,35 @@
+﻿// ----------------------------------------------------------------------------
+// The Proprietary or MIT License
+// Copyright (c) 2022-2022 RFS_6ro <rfs6ro@gmail.com>
+// ----------------------------------------------------------------------------
+
+using Core.ECS.Tags.InputListener;
+using Initialization.Configs.Input;
+using Unity.Entities;
+using Update.Spawners.Unit.Initializers;
+
+namespace Update.Input
+{
+    [UpdateAfter(typeof(SAliensInitializer))]
+    public partial class SReadKeyboardInput : SystemBase
+    {
+        protected override void OnUpdate()
+        {
+            CInputConfig cInputConfig = SystemAPI.GetSingleton<CInputConfig>();
+            Entities
+                .WithAll<CInputListener>()
+                .ForEach((ref CFrameInputData frameInputData) =>
+                {
+                    frameInputData.Acceleration = UnityEngine.Input.GetKey(cInputConfig.AccelerationKey);
+
+                    frameInputData.IsShotRequested = UnityEngine.Input.GetKey(cInputConfig.ShootKey);
+
+                    float rotation = 0;
+                    rotation += UnityEngine.Input.GetKey(cInputConfig.TurnLeftKey) ? -1f : 0f;
+                    rotation += UnityEngine.Input.GetKey(cInputConfig.TurnRightKey) ? 1f : 0f;
+                    frameInputData.Rotation = rotation;
+                })
+                .Run();
+        }
+    }
+}
