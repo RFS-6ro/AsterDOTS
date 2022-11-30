84e42d2b9983b5673d01f0c4ae3d5ac389186d2b
dtsyvakin <dtsyvakin@gear-games.com>
2022-11-30 04:40:40 +0300

    A-19 Fix: Push missing Spawn points config

---
+++ AsterDOTS/Assets/Initialization/Configs/SpawnPoints/CSpawnPointsConfigBaker.cs
@@ -0,0 +1,25 @@
+﻿// ----------------------------------------------------------------------------
+// The Proprietary or MIT License
+// Copyright (c) 2022-2022 RFS_6ro <rfs6ro@gmail.com>
+// ----------------------------------------------------------------------------
+
+using System.Linq;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+
+namespace Initialization.Configs.SpawnPoints
+{
+    public class CSpawnPointsConfigBaker : Baker<CSpawnPointsConfigAuthority>
+    {
+        public override void Bake(CSpawnPointsConfigAuthority auth)
+        {
+            AddComponent(new CSpawnPointsConfig
+            {
+                Count = auth.Points.Count(),
+                Points = new NativeArray<float3>(auth.Points.ToArray(), Allocator.Persistent),
+                Forwards = new NativeArray<float3>(auth.Forwards.ToArray(), Allocator.Persistent)
+            });
+        }
+    }
+}
